f243d68a07c6d168a5e32235174a5b80b785b456
LauTsangFai <jayday@aliyun.com>
2020-12-24 23:10:48 +0800

    null

--- pages/chart/chart.vue
+++ pages/chart/chart.vue
@@ -6,20 +6,17 @@
 			<template v-for="(item,index) in list">
 				<chatList :item="item" :index="index" :pretime="index > 0 ? list[index-1].create_time : 0"> </chatList>
 			</template>
-		
-			<view id='tobb' class="mt-1 text-center bg-white" :style="'height:'+ transHeight+ 'px;'" >
+			<view id='tobb' class="mt-1  bg-white" :style="'height:'+ transHeight+ 'px;'" >
 			</view>
-			<uni-popup type="bottom" ref='popup'>
-				<view class="mt-1 text-center bg-white" :style="'height:'+ transHeight+ 'px;'" ></view>
-			</uni-popup>
 		</scroll-view>
 
 		
 
 		<!-- 底部操作条 -->
-		<view style="height: 100rpx; left: 0; right: 0; position: fixed;" :style="'bottom:'+ inputBottom + 'px;'" class=" flex align-center border-top bg-white">
+		<view style="height: 100rpx; left: 0; right: 0; position: fixed;" :style="'bottom:'+ transHeight + 'px;'" class=" flex align-center border-top bg-white">
 
-			<input type="text" v-model="inputText" class="flex-1 rounded bg-light ml-2  " @focus="pageToBottom" style="padding: 10rpx; "
+			<input type="text" v-model="inputText" class="flex-1 rounded bg-light ml-2  " 
+			 style="padding: 10rpx; "
 			 placeholder="文明发言" :adjust-position="false" />
 
 			<view class="iconfont icon-fabu flex align-center justify-center font-lg animate__animated animate__jello"
@@ -36,11 +33,11 @@
 
 <script>
 	import chatList from '@/components/chatList/chatList.vue'
-	import uniPopup from '@/components/uni-popup/uni-popup.vue'
+	import uniTransition from '@/components/uni-transition/uni-transition.vue'
 	export default {
 		components: {
 			chatList,
-			uniPopup
+			uniTransition
 		},
 		data() {
 			return {
@@ -111,26 +108,34 @@
 						create_time: 1570783530
 					}, {
 						user_id: 2,
-					avatar: "/static/demo/userpic/6.jpg",
+						avatar: "/static/demo/userpic/6.jpg",
 						username: "昵称",
-						data: "圣诞快乐",
+						data: "(⊙o⊙)…",
 						type: "text",
 						create_time: 1570783530
 					}, {
 						user_id: 1,
 						avatar: "/static/default.jpg",
 						username: "昵称",
-						data: "因为你,因为我",
+						data: "节日快乐",
 						type: "text",
 						create_time: 1570783530
 					}, {
-						user_id: 1,
-						avatar: "/static/default.jpg",
+						user_id: 2,
+						avatar: "/static/demo/userpic/6.jpg",
 						username: "昵称",
-						data: "/static/female.jpg",
+						data: "/static/1224.jpg",
 						type: "img",
-						create_time: 1590783530
+						create_time: (new Date()).getTime()
 					},
+					{
+						user_id: 1,
+						avatar: "/static/default.jpg",
+						username: "昵称",
+						data: "平安夜快乐",
+						type: "text",
+						create_time: (new Date()).getTime()
+					}
 				]
 			}
 		},
@@ -178,7 +183,7 @@
 						})
 				    }
 				});
-			}
+			},
 		},
 		mounted() {
 			
@@ -187,17 +192,13 @@
 			this.pageToBottom()
 		},
 		onLoad(e) {
+		// 	console.log(e.info);
 			uni.onKeyboardHeightChange(res => {
 		
 				this.transHeight= res.height
+				
 				this.$nextTick(()=>{
-					
-				this.inputBottom = res.height
-				if(res.height > 0)
-				this.$refs.popup.open()
-				else
-				this.$refs.popup.close()
-				this.pageToBottom()
+					this.pageToBottom()
 				})
 			
 			})
